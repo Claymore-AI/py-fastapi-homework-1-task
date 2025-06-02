b3c5dcabae0942c50065bc0ba03a1211e1cfa514
Claymore-AI <dimalishchuk2003@gmail.com>
2025-06-02 17:52:06 +0300

    fix base url

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -36,7 +36,7 @@ async def get_movies(
     )
     movies = result.scalars().all()
 
-    base_url = "/api/v1/theater/movies/"
+    base_url = "/movies/"
     prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
     next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
 
